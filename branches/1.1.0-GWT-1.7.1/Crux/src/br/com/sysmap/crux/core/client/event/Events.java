85274164e462750373f2257bd4bbce8c4fb79242
tr_bustamante@yahoo.com.br <tr_bustamante@yahoo.com.br@a5d2bbaa-053c-11de-b17c-0f1ef23b492c>
2009-12-17 15:49:07 +0000

    MANAGEMENT - Compatible version with GWT 1.7.1
    git-svn-id: http://crux-framework.googlecode.com/svn@1178 a5d2bbaa-053c-11de-b17c-0f1ef23b492c

---
+++ branches/1.1.0-GWT-1.7.1/Crux/src/br/com/sysmap/crux/core/client/event/Events.java
@@ -0,0 +1,217 @@
+/*
+ * Copyright 2009 Sysmap Solutions Software e Consultoria Ltda.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package br.com.sysmap.crux.core.client.event;
+
+import br.com.sysmap.crux.core.client.Crux;
+import br.com.sysmap.crux.core.client.screen.InterfaceConfigException;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.shared.GwtEvent;
+
+/**
+ * 
+ * @author Thiago da Rosa de Bustamante <code>tr_bustamante@yahoo.com.br</code>
+ */
+public class Events 
+{
+	public static final String SYNC_TYPE_SYNCHRONOUS = "synchronous";
+	public static final String SYNC_TYPE_ASSYNCHRONOUS = "assynchronous";
+	
+	public static final String EVENT_LOAD = "onload";
+	public static final String EVENT_CLOSE = "onclose";
+	public static final String EVENT_CLOSING = "onclosing";
+	public static final String EVENT_RESIZED = "onresized";
+	public static final String EVENT_LOAD_IMAGES = "onloadimage";
+	public static final String EVENT_LOAD_ORACLE = "onloadoracle";
+	public static final String EVENT_LOAD_FORMAT = "onloadformat";
+	public static final String EVENT_LOAD_WIDGET = "onloadwidget";
+	public static final String EVENT_EXECUTE_EVENT = "onexecute";
+	public static final String EVENT_SUBMIT_COMPLETE = "onsubmitcomplete";
+	public static final String EVENT_SUBMIT = "onsubmit";
+	public static final String EVENT_HISTORY_CHANGED = "onhistorychanged";
+	
+	private static RegisteredClientEventHandlers registeredClientEventHandlers;
+	
+	
+	/**
+	 * Create an event object.
+	 * @param evtId
+	 * @param evt
+	 * @return
+	 */
+	public static Event getEvent(String evtId, String evt)
+	{
+		try
+		{
+			if (evtId != null && evtId.trim().length() > 0 && evt != null && evt.trim().length() > 0)
+			{
+				int dotPos = evt.indexOf('.');
+				if (dotPos > 0 && dotPos < evt.length()-1)
+				{
+					String evtHandler = evt.substring(0, dotPos);
+					final String method = evt.substring(dotPos+1);				
+					return new Event(evtId, evtHandler, method);
+				}
+				else
+				{
+					throw new EventException(Crux.getMessages().eventFactoryInvalidHandlerMethodDeclaration(evt));
+				}
+			}
+		}
+		catch (Throwable e)
+		{
+			Crux.getErrorHandler().handleError(e);
+		}
+		return null;
+	}
+
+	/**
+	 * Dispatch an event call.
+	 * @param event
+	 * @param sourceEvent
+	 * @return
+	 */
+	public static Object callEvent(Event event, CruxEvent<?> sourceEvent)
+	{
+		return callEvent(event, sourceEvent, false);
+	}
+	
+	/**
+	 * Dispatch an event call.
+	 * @param event
+	 * @param sourceEvent
+	 * @param fromOutOfModule
+	 * @return
+	 */
+	public static Object callEvent(Event event, CruxEvent<?> sourceEvent, boolean fromOutOfModule)
+	{
+		try 
+		{
+			EventProcessor processor = createEventProcessor(event);
+			processor.processEvent(sourceEvent, fromOutOfModule);
+			return processEventResult(event, processor);
+		}
+		catch (InterfaceConfigException e) 
+		{
+			Crux.getErrorHandler().handleError(e);
+		}
+		return null;
+	}
+	
+	/**
+	 * Dispatch an event call.
+	 * @param event
+	 * @param sourceEvent
+	 * @return
+	 */
+	public static Object callEvent(Event event, GwtEvent<?> sourceEvent)
+	{
+		try 
+		{
+			EventProcessor processor = createEventProcessor(event);
+			processor.processEvent(sourceEvent);
+			return processEventResult(event, processor);
+		}
+		catch (InterfaceConfigException e) 
+		{
+			Crux.getErrorHandler().handleError(e);
+		}
+		return null;
+	}
+
+	/**
+	 * 
+	 * @param event
+	 * @param processor
+	 * @return
+	 */
+	protected static Object processEventResult(Event event, EventProcessor processor)
+	{
+		if (processor.hasException())
+		{
+			Crux.getErrorHandler().handleError(Crux.getMessages().eventProcessorClientError(event.getController()+"."+event.getMethod(), 
+					processor.exception().getLocalizedMessage()), processor.exception());
+		}
+		else if (processor.validationMessage() != null)
+		{
+			Crux.getValidationErrorHandler().handleValidationError(processor.validationMessage());
+		}
+		else if (processor.hasReturn())
+		{
+			return processor.returnValue();
+		}
+		return null;
+	}
+	
+	/**
+	 * Create a EventProcessor for the event.
+	 * @param event
+	 * @return
+	 * @throws InterfaceConfigException
+	 */
+	protected static EventProcessor createEventProcessor(final Event event) throws InterfaceConfigException
+	{
+		getRegisteredClientEventHandlers();
+
+		return new EventProcessor()
+		{
+			public void processEvent(GwtEvent<?> sourceEvent)
+			{
+				final EventClientHandlerInvoker handler = (EventClientHandlerInvoker)getRegisteredClientEventHandlers().getEventHandler(event.getController());
+				if (handler == null)
+				{
+					Crux.getErrorHandler().handleError(Crux.getMessages().eventProcessorClientControllerNotFound(event.getController()));
+					return;
+				}
+				try
+				{
+					handler.invoke(event.getMethod(), sourceEvent, this);
+				}
+				catch (Exception e) 
+				{
+					_exception = e;
+				}
+			}
+
+			public void processEvent(CruxEvent<?> sourceEvent, boolean fromOutOfModule)
+			{
+				final EventClientHandlerInvoker handler = (EventClientHandlerInvoker)getRegisteredClientEventHandlers().getEventHandler(event.getController());
+				if (handler == null)
+				{
+					Crux.getErrorHandler().handleError(Crux.getMessages().eventProcessorClientControllerNotFound(event.getController()));
+					return;
+				}
+				try
+				{
+					handler.invoke(event.getMethod(), sourceEvent, fromOutOfModule, this);
+				}
+				catch (Exception e) 
+				{
+					_exception = e;
+				}
+			}
+		};
+	}
+
+	protected static RegisteredClientEventHandlers getRegisteredClientEventHandlers()
+	{
+		if (registeredClientEventHandlers == null)
+		{
+			registeredClientEventHandlers = (RegisteredClientEventHandlers)GWT.create(RegisteredClientEventHandlers.class);
+		}
+		return registeredClientEventHandlers;
+	}
+}
---
+++ branches/1.1.0-GWT-1.7.1/CruxHtmlTags/src/br/com/sysmap/crux/tools/htmltags/template/TemplatesScanner.java
@@ -0,0 +1,251 @@
+/*
+ * Copyright 2009 Sysmap Solutions Software e Consultoria Ltda.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package br.com.sysmap.crux.tools.htmltags.template;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.w3c.dom.Document;
+
+import br.com.sysmap.crux.core.i18n.MessagesFactory;
+import br.com.sysmap.crux.core.server.scan.ScannerURLS;
+import br.com.sysmap.crux.core.utils.RegexpPatterns;
+import br.com.sysmap.crux.scannotation.archiveiterator.Filter;
+import br.com.sysmap.crux.scannotation.archiveiterator.IteratorFactory;
+import br.com.sysmap.crux.tools.htmltags.HTMLTagsMessages;
+
+/**
+ * 
+ * @author Thiago da Rosa de Bustamante <code>tr_bustamante@yahoo.com.br</code>
+ *
+ */
+public class TemplatesScanner 
+{
+	private static final Log logger = LogFactory.getLog(TemplatesScanner.class);
+	private static final TemplatesScanner instance = new TemplatesScanner();
+	private HTMLTagsMessages messages = MessagesFactory.getMessages(HTMLTagsMessages.class);
+	private DocumentBuilder documentBuilder;
+	private static URL[] urlsForSearch = null;
+	private static final Lock lock = new ReentrantLock(); 
+	
+	/**
+	 * 
+	 */
+	private TemplatesScanner() 
+	{
+		try
+		{
+			DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
+			documentBuilderFactory.setNamespaceAware(true);
+			this.documentBuilder = documentBuilderFactory.newDocumentBuilder();
+		}
+		catch (ParserConfigurationException e)
+		{
+			throw new TemplateException(messages.templatesScannerErrorBuilderCanNotBeCreated(), e);
+		}
+	}
+	
+	protected transient String[] ignoredPackages = {"javax", "java", "sun", "com.sun", "org.apache", 
+													"net.sf.saxon", "javassist", "org.json", "com.extjs",
+													"com.metaparadigm", "junit"};
+
+	/**
+	 * 
+	 * @return
+	 */
+	public String[] getIgnoredPackages()
+	{
+		return ignoredPackages;
+	}
+
+	/**
+	 * 
+	 * @param ignoredPackages
+	 */
+	public void setIgnoredPackages(String[] ignoredPackages)
+	{
+		this.ignoredPackages = ignoredPackages;
+	}
+
+	/**
+	 * 
+	 * @param urls
+	 */
+	private void scanArchives(URL... urls)
+	{
+		for (URL url : urls)
+		{
+			if(existsFile(url))
+			{
+				Filter filter = new Filter()
+				{
+					public boolean accepts(String fileName)
+					{
+						if (fileName.endsWith(".template.xml"))
+						{
+							if (fileName.startsWith("/")) fileName = fileName.substring(1);
+							if (!ignoreScan(fileName.replace('/', '.')))
+							{
+								Document template;
+								try
+								{
+									InputStream inputStream = getClass().getResourceAsStream(fileName);
+									if (inputStream != null)
+									{
+										template = documentBuilder.parse(inputStream);
+									}
+									else
+									{
+										inputStream = getClass().getResourceAsStream("/"+fileName);
+										if (inputStream != null)
+										{
+											template = documentBuilder.parse(inputStream);
+										}
+										else
+										{
+											template = documentBuilder.parse(new File(fileName));
+										}
+									}
+									Templates.registerTemplate(getTemplateId(fileName), template);
+								}
+								catch (Exception e)
+								{
+									logger.error(messages.templatesScannerErrorParsingTemplateFile(fileName), e);
+									return false;
+								}
+								return true;
+							}
+						}
+						return false;
+					}
+				};
+	
+				try
+				{
+					IteratorFactory.create(url, filter);
+				}
+				catch (IOException e)
+				{
+					throw new TemplateException(messages.templatesScannerInitializationError(e.getLocalizedMessage()), e);
+				}
+			}
+		}
+	}
+	
+	/**
+	 * Returns true if exists a file or directory with the given URL
+	 * @param url
+	 * @return
+	 * @throws IOException 
+	 */
+	private boolean existsFile(URL url) throws RuntimeException
+	{
+		try
+		{
+			File file = new File(url.toURI());
+			return file.exists();
+		}
+		catch (URISyntaxException e)
+		{
+			throw new TemplateException(messages.templatesScanningURLError(url.toString(), e.getLocalizedMessage()), e);
+		}
+	}
+
+	/**
+	 * 
+	 */
+	public void scanArchives()
+	{
+		if (urlsForSearch == null)
+		{
+			initialize(ScannerURLS.getWebURLsForSearch());
+		}
+		scanArchives(urlsForSearch);
+	}
+	
+	/**
+	 * 
+	 * @param urls
+	 */
+	public static void initialize(URL[] urls)
+	{
+		lock.lock();
+		try
+		{
+			urlsForSearch = urls;
+		}
+		finally
+		{
+			lock.unlock();
+		}
+	}
+	
+	/**
+	 * 
+	 * @param fileName
+	 * @return
+	 */
+	private String getTemplateId(String fileName)
+	{
+		fileName = fileName.substring(0, fileName.length() - 13);
+		fileName = RegexpPatterns.REGEXP_BACKSLASH.matcher(fileName).replaceAll("/");
+		int indexStartId = fileName.lastIndexOf('/');
+		if (indexStartId > 0)
+		{
+			fileName = fileName.substring(indexStartId+1);
+		}
+		
+		return fileName;
+	}
+	
+	/**
+	 * 
+	 * @param intf
+	 * @return
+	 */
+	private boolean ignoreScan(String intf)
+	{
+		for (String ignored : ignoredPackages)
+		{
+			if (intf.startsWith(ignored + "."))
+			{
+				return true;
+			}
+		}
+		return false;
+	}
+
+	/**
+	 * 
+	 * @return
+	 */
+	public static TemplatesScanner getInstance()
+	{
+		return instance;
+	}
+	
+}
