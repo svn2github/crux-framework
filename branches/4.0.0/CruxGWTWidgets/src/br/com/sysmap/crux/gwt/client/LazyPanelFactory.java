a96482e8e3ea703a51dde8dca6de35539010e31e
thiago@cruxframework.org <thiago@cruxframework.org@a5d2bbaa-053c-11de-b17c-0f1ef23b492c>
2010-11-02 14:41:58 +0000

    ENHANCEMENT - Issue #218
    git-svn-id: http://crux-framework.googlecode.com/svn@2240 a5d2bbaa-053c-11de-b17c-0f1ef23b492c

--- branches/4.0.0/CruxGWTWidgets/src/br/com/sysmap/crux/gwt/client/LazyPanelFactory.java
+++ branches/4.0.0/CruxGWTWidgets/src/br/com/sysmap/crux/gwt/client/LazyPanelFactory.java
@@ -15,103 +15,30 @@
  */
 package br.com.sysmap.crux.gwt.client;
 
-import br.com.sysmap.crux.core.client.Crux;
 import br.com.sysmap.crux.core.client.declarative.DeclarativeFactory;
-import br.com.sysmap.crux.core.client.screen.CruxLazyPanel;
-import br.com.sysmap.crux.core.client.screen.HasWidgetsHandler;
 import br.com.sysmap.crux.core.client.screen.InterfaceConfigException;
-import br.com.sysmap.crux.core.client.screen.LazyFactory;
 import br.com.sysmap.crux.core.client.screen.parser.CruxMetaData;
 
-import com.google.gwt.dom.client.Element;
 import com.google.gwt.user.client.ui.LazyPanel;
-import com.google.gwt.user.client.ui.Widget;
 
 /**
  * A Panel which content is only rendered when it becomes visible for the first time.
  * 
  * @author Thiago da Rosa de Bustamante
  */
-@DeclarativeFactory(id="lazyPanel", library="gwt", lazy=true)
-public class LazyPanelFactory extends PanelFactory<LazyPanel> implements LazyFactory<LazyPanel>
+@DeclarativeFactory(id="lazyPanel", library="gwt")
+public class LazyPanelFactory extends PanelFactory<LazyPanel> 
 {
 
-	/**
-	 * @see br.com.sysmap.crux.core.client.screen.HasWidgetsFactory#add(com.google.gwt.user.client.ui.Widget, com.google.gwt.user.client.ui.Widget, com.google.gwt.dom.client.Element, com.google.gwt.dom.client.Element)
-	 */
-	public void add(LazyPanel parent, Widget child, Element parentElement, Element childElement) throws InterfaceConfigException
-	{
-		parent.add(child);
-	}
-	
-	/**
-	 * @see br.com.sysmap.crux.core.client.screen.WidgetFactory#instantiateWidget(com.google.gwt.dom.client.Element, java.lang.String)
-	 */
 	@Override
-	public LazyPanel instantiateWidget(final CruxMetaData element, String widgetId) 
+	public void processAttributes(WidgetFactoryContext<LazyPanel> context) throws InterfaceConfigException 
 	{
-		if (Crux.getConfig().enableRuntimeLazyWidgetsInitialization())
-		{
-			//maybeBuildLazyDependencyList(element, widgetId);
-		}		
-		
-//		LazyPanel result =  new CruxLazyPanel(element.getInnerHTML(), widgetId);
-		LazyPanel result =  new CruxLazyPanel("", widgetId);
-		//element.setInnerHTML("");
-		HasWidgetsHandler.handleWidgetElement(result, widgetId, "gwt_lazyPanel");
-		return result;
-	}
-
-	/**
-	 * @param element
-	 * @param externalId
-	 * @return
-	 *
-	private Element getParentLazyPanelElement(Element element, String externalId) 
-	{
-		Element elementParent = element.getParentElement();
-		while (elementParent != null && (elementParent.getId() == null || !elementParent.getId().equals(externalId)))
-		{
-			String type = elementParent.getAttribute("_type");
-			if (type != null && StringUtils.unsafeEquals(type,"gwt_lazyPanel"))//TODO: pegar todos os possiveis lazy, via um generator
-			{
-				return elementParent;
-			}
-			if (StringUtils.unsafeEquals("BODY",elementParent.getTagName()))
-			{
-				return null;
-			}
-			elementParent = elementParent.getParentElement();
-		}
-			
-		return elementParent;	
+		super.processAttributes(context);
 	}
 	
-	/**
-	 * @param element
-	 * @param widgetId
-	 *
-	private void maybeBuildLazyDependencyList(final Element element, String widgetId)
+	@Override
+	public LazyPanel instantiateWidget(CruxMetaData metaElem, String widgetId) throws InterfaceConfigException 
 	{
-		if (Crux.getConfig().enableRuntimeLazyWidgetsInitialization())
-		{
-			if (getParentLazyPanelElement(element, widgetId) == null)
-			{
-				// Only the most external lazy panels must be initialized, once they also
-				// initialize their internal lazy children.
-				NodeList<Element> spanElements = element.getElementsByTagName("SPAN");
-
-				int spansLength = spanElements.getLength();
-				for (int i=0; i<spansLength; i++)
-				{
-					Element spanElement = spanElements.getItem(i);
-					if (isValidCruxWidgetMetaTag(spanElement))
-					{
-						Element parentLazyWidget = getParentLazyPanelElement(spanElement, widgetId);
-						addLazyWidgetDependency(spanElement.getId(), parentLazyWidget.getId());
-					}
-				}
-			}
-		}
-	}*/	
+		return br.com.sysmap.crux.core.client.screen.LazyPanelFactory.getLazyPanel(metaElem, widgetId);
+	}
 }
