919f1159ce3f5aa6d00f3693216d11e40971834d
thiago@cruxframework.org <thiago@cruxframework.org@a5d2bbaa-053c-11de-b17c-0f1ef23b492c>
2011-01-21 19:44:53 +0000

    ENHANCEMENT - Issue #229
    git-svn-id: http://crux-framework.googlecode.com/svn@2418 a5d2bbaa-053c-11de-b17c-0f1ef23b492c

--- branches/4.0.0/CruxGadgets/src/br/com/sysmap/crux/gadget/client/widget/GadgetViewFactory.java
+++ branches/4.0.0/CruxGadgets/src/br/com/sysmap/crux/gadget/client/widget/GadgetViewFactory.java
@@ -24,11 +24,11 @@ import br.com.sysmap.crux.core.client.declarative.TagChildAttributes;
 import br.com.sysmap.crux.core.client.declarative.TagChildren;
 import br.com.sysmap.crux.core.client.screen.HTMLContainer;
 import br.com.sysmap.crux.core.client.screen.InterfaceConfigException;
-import br.com.sysmap.crux.core.client.screen.children.WidgetChildProcessor;
-import br.com.sysmap.crux.core.client.screen.children.WidgetChildProcessor.AnyTag;
 import br.com.sysmap.crux.core.client.screen.parser.CruxMetaDataElement;
 import br.com.sysmap.crux.core.rebind.widget.WidgetCreator;
 import br.com.sysmap.crux.core.rebind.widget.WidgetCreatorContext;
+import br.com.sysmap.crux.core.rebind.widget.creator.children.WidgetChildProcessor;
+import br.com.sysmap.crux.core.rebind.widget.creator.children.WidgetChildProcessor.AnyTag;
 import br.com.sysmap.crux.gadget.client.widget.GadgetView.View;
 import br.com.sysmap.crux.gwt.client.AbstractHTMLPanelFactory;
 
