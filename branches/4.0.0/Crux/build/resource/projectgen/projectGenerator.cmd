354245d0a45faee3c5fa7f1cf5a6b26d5880484e
thiago@cruxframework.org <thiago@cruxframework.org@a5d2bbaa-053c-11de-b17c-0f1ef23b492c>
2011-03-22 20:34:08 +0000

    IGNORE - Project Layout
    git-svn-id: http://crux-framework.googlecode.com/svn@2586 a5d2bbaa-053c-11de-b17c-0f1ef23b492c

---
+++ branches/4.0.0/Crux/build/resource/projectgen/projectGenerator.cmd
@@ -0,0 +1,2 @@
+@java -cp "./;./lib/build/crux-dev-deps.jar;./lib/build/crux-dev.jar;./lib/build/gwt-dev.jar;./lib/build/gwt-user.jar;" br.com.sysmap.crux.tools.projectgen.CruxProjectGenerator %*
+
--- branches/4.0.0/Crux/src/tools/br/com/sysmap/crux/tools/projectgen/Launcher.java
+++ branches/4.0.0/Crux/src/tools/br/com/sysmap/crux/tools/projectgen/Launcher.java
@@ -132,20 +132,22 @@ public class Launcher
 	    return contextHandler;
     }
 	
+	/**
+	 * @throws Exception
+	 */
 	protected void installQuickStartApp() throws Exception
 	{
 		File webAppLib = new File("./apps/quickstart.war/WEB-INF/lib");
 		File buildLib = new File("./lib/build");
 		File webInfLib = new File("./lib/web-inf");
-		File modulesBuildLib = new File("./lib/modules/build");
-		File modulesWebInfLib = new File("./lib/modules/web-inf");
 		
 		FileUtils.copyFilesFromDir(buildLib, webAppLib);
 		FileUtils.copyFilesFromDir(webInfLib, webAppLib);
-		FileUtils.copyFilesFromDir(modulesBuildLib, webAppLib);
-		FileUtils.copyFilesFromDir(modulesWebInfLib, webAppLib);
 	}
 	
+	/**
+	 * @throws Exception
+	 */
 	protected void installHelloWorldApp() throws Exception
 	{
 		File webAppLib = new File("./apps/helloworld.war/WEB-INF/lib");
