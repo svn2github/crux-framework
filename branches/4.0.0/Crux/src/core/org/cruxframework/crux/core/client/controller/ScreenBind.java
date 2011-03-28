7c10422612751e92b979f072ab1c7b1887087922
thiago@cruxframework.org <thiago@cruxframework.org@a5d2bbaa-053c-11de-b17c-0f1ef23b492c>
2011-03-28 21:01:50 +0000

    IGNORE - Javadoc for classes
    git-svn-id: http://crux-framework.googlecode.com/svn@2679 a5d2bbaa-053c-11de-b17c-0f1ef23b492c

--- branches/4.0.0/Crux/src/core/org/cruxframework/crux/core/client/controller/ScreenBind.java
+++ branches/4.0.0/Crux/src/core/org/cruxframework/crux/core/client/controller/ScreenBind.java
@@ -21,12 +21,36 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * @author Thiago da Rosa de Bustamante
+ * This annotation can be used to customize the binding of a {@link ValueObject} field with 
+ * a screen widget.
+ * <p>
+ * It only makes any effect if used on fields of a {@link ValueObject} class.
+ * <p>
+ * See the following example:
+ * <pre>
+ * {@code@}{@link ValueObject}(bindWidgetByFieldName=false)
+ *public class Person
+ *{
+ *       {@code@}ScreenBind("person.name")
+ *       private String name;
  *
+ *       {@code@}@ScreenBind
+ *       private String phone; //will be bound to "phone" widget
+ *       
+ *       private String address; // will not be bound.
+ *       ...//getters and setters
+ *}
+ * </pre>
+ * @author Thiago da Rosa de Bustamante
+ * @see ValueObject
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.FIELD)
 public @interface ScreenBind
 {
+	/**
+	 * The name of the widget on the screen that will be bound to the annotated field. 
+	 * If empty, the field name is used. 
+	 */
 	String value() default "";
 }
--- branches/4.0.0/Crux/src/core/org/cruxframework/crux/core/client/controller/ValueObject.java
+++ branches/4.0.0/Crux/src/core/org/cruxframework/crux/core/client/controller/ValueObject.java
@@ -21,7 +21,6 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 import org.cruxframework.crux.core.client.screen.Screen;
-//TODO: terminar essa documentacao
 /**
  * This annotation can be used to bind a Data Object to the {@link Screen}.
  * <p>
@@ -31,22 +30,70 @@ import org.cruxframework.crux.core.client.screen.Screen;
  * After the event handler method finish, the screen is updated with any change made on the 
  * tied object.
  *<p>
- * For example, see the following controller:
+ * See the following example:
  *
  * <pre>
- * {@code @Controller}("myController")
- * public class MyController
- * {
- *    {@code @}{@link Expose}
- *    public void myEventHandler()
- *    {
- *    	Window.alert("event dispatched!");
- *    }
- * }
+ *{@code @}ValueObject
+ *public class Person
+ *{
+ *       private String name;
+ *       private String phone;
+ *       
+ *       public String getName() {
+ *               return name;
+ *       }
+ *       public void setName(String name) {
+ *               this.name = name;
+ *       }
+ *       public String getPhone() {
+ *               return phone;
+ *       }
+ *       public void setPhone(String phone) {
+ *               this.phone = phone;
+ *       }
+ *} 
  * </pre>
- * 
+ * <p>
+ * <pre>
+ *{@code @}{@link Controller}("myController")
+ *public class MyClass
+ *{
+ *   {@code @}{@link Create}
+ *   protected Person person;
+ *
+ *  {@code @}{@link Expose}
+ *   public void myMethod()
+ *   {
+ *       Window.alert(person.getName());
+ *       person.setPhone("1234-5678");
+ *   }
+ *}
+ *</pre> 
+ *<p>
+ *<pre>
+ *{@code <html xmlns="http://www.w3.org/1999/xhtml" 
+ *     xmlns:crux="http://www.cruxframework.org/crux" 
+ *     xmlns:gwt="http://www.cruxframework.org/crux/gwt">}
+ *  {@code <head>}
+ *      {@code <script language="javascript" src="cruxtest/cruxtest.nocache.js"></script>}
+ *  {@code </head>}
+ *  {@code <body>}
+ *      {@code <crux:screen useController="myController" >}
+ *           {@code <gwt:textBox id="name" />}
+ *           {@code <gwt:textBox id="phone" />}
+ *           {@code <gwt:button id="myButton" text="Hello" onClick="myController.myMethod" />}
+ *      {@code </crux:screen>}
+ *  {@code </body>}
+ *{@code </html>}
+ *</pre>
+ *<p>
+ *In the above example, the value of the "name" textBox on page will be bound to field 
+ *"name" of the Person object created by controller (the same is true to "phone"). 
+ *<p>
+ *After the handler execution, the changes made in the value object will be reflected on page. 
  * @see Controller  
  * @see Create  
+ * @see ScreenBind 
  * @author Thiago da Rosa de Bustamante
  *
  */
@@ -54,5 +101,10 @@ import org.cruxframework.crux.core.client.screen.Screen;
 @Target(ElementType.TYPE)
 public @interface ValueObject
 {
+	/**
+     * Setting this value to false makes Crux to does not bind all value object fields to widgets 
+     * automatically. If you set this, you must specify for each field, the name of the widget 
+     * that it will be bound (through @ScreenBind annotation).	 
+     */
 	boolean bindWidgetByFieldName() default true;
 }
