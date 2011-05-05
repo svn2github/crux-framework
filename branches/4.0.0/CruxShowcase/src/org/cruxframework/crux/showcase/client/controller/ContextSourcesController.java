b378f21413b41a339c294a9ce3ceed74444ca5b1
thiago@cruxframework.org <thiago@cruxframework.org@a5d2bbaa-053c-11de-b17c-0f1ef23b492c>
2011-05-05 19:22:25 +0000

    showcase examples
    git-svn-id: http://crux-framework.googlecode.com/svn@2794 a5d2bbaa-053c-11de-b17c-0f1ef23b492c

--- branches/4.0.0/CruxShowcase/src/org/cruxframework/crux/showcase/client/controller/ContextSourcesController.java
+++ branches/4.0.0/CruxShowcase/src/org/cruxframework/crux/showcase/client/controller/ContextSourcesController.java
@@ -1,42 +1,20 @@
 package org.cruxframework.crux.showcase.client.controller;
 
+import java.util.ArrayList;
+
 import org.cruxframework.crux.core.client.controller.Controller;
-import org.cruxframework.crux.core.client.controller.Expose;
 
 @Controller("contextSourcesController")
-public class ContextSourcesController extends SourcesController {
+public class ContextSourcesController extends BaseSourcesController implements ContextSourcesControllerCrossDoc {
 	
-	private boolean xmlLoaded;
-	private boolean frameControllerLoaded;
-	private boolean contextInitializationControllerLoaded;
-	
-	@Expose
-	public void loadFrameXML() {
-		
-		if(!xmlLoaded)
-		{
-			xmlLoaded = true;
-			loadXMLFile("contextFrame.crux.xml", "frameXml");
-		}
-	}
-	
-	@Expose
-	public void loadFrameController() {
-		
-		if(!frameControllerLoaded)
-		{
-			frameControllerLoaded = true;
-			loadFile("client/controller/ContextFrameController.java", "frameController");
-		}
-	}
-	
-	@Expose
-	public void loadContextInitializer() {
-		
-		if(!contextInitializationControllerLoaded)
-		{
-			contextInitializationControllerLoaded = true;
-			loadFile("client/controller/ContextInitializerController.java", "contextInitializerCntr");
-		}
-	}
+	/**
+	 * @see org.cruxframework.crux.showcase.client.controller.BaseSourcesController#getAdditionalSources()
+	 */
+	protected ArrayList<SourceTab> getAdditionalSources() {
+		ArrayList<SourceTab> aditionalTabs = new ArrayList<SourceTab>();
+		aditionalTabs.add(new SourceTab("client/controller/ContextFrameController.java", "Internal Frame Controller", true));
+		aditionalTabs.add(new SourceTab("client/controller/ContextInitializerController.java", "Context Initializer Controller", true));
+		aditionalTabs.add(new SourceTab("contextFrame.crux.xml", "Internal Frame Page", true));
+		return aditionalTabs;
+	}	
 }
