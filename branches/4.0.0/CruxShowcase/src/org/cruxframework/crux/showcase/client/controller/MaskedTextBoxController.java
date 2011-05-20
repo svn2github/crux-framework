33b10d9ba948ed1b387c243d952db5cf0e45bc47
gesse@cruxframework.org <gesse@cruxframework.org@a5d2bbaa-053c-11de-b17c-0f1ef23b492c>
2011-05-20 23:06:21 +0000

    ENHANCEMENT - Issue #235 Issue #133
    git-svn-id: http://crux-framework.googlecode.com/svn@2867 a5d2bbaa-053c-11de-b17c-0f1ef23b492c

--- branches/4.0.0/CruxShowcase/src/org/cruxframework/crux/showcase/client/controller/MaskedTextBoxController.java
+++ branches/4.0.0/CruxShowcase/src/org/cruxframework/crux/showcase/client/controller/MaskedTextBoxController.java
@@ -4,12 +4,9 @@ import org.cruxframework.crux.core.client.controller.Controller;
 import org.cruxframework.crux.core.client.controller.Create;
 import org.cruxframework.crux.core.client.controller.Expose;
 import org.cruxframework.crux.core.client.formatter.Formatter;
-import org.cruxframework.crux.core.client.formatter.InvalidFormatException;
-import org.cruxframework.crux.core.client.formatter.annotation.FormatterName;
 import org.cruxframework.crux.core.client.screen.Screen;
 import org.cruxframework.crux.core.client.screen.ScreenWrapper;
 import org.cruxframework.crux.widgets.client.maskedtextbox.MaskedTextBox;
-import org.cruxframework.crux.widgets.client.maskedtextbox.MaskedTextBoxBaseFormatter;
 
 import com.google.gwt.user.client.ui.Label;
 
@@ -33,33 +30,7 @@ public class MaskedTextBoxController {
 		maskedTextBox.setFormatter(newFormatter);		
 		
 		swapLabel();
-	}
-
-	@FormatterName("phone")
-	public static class PhoneFormatter extends MaskedTextBoxBaseFormatter{
-		
-		public String getMask(){
-			return "(999)999-9999";
-		}
-
-		public String format(Object input){
-			if (input == null || !(input instanceof String) || ((String)input).length() != 10){
-				return "";
-			}
-			
-			String strInput = (String) input;
-			return "("+strInput.substring(0,3)+")"+strInput.substring(3,6)+"-"+strInput.substring(6);
-		}
-
-		public Object unformat(String input) throws InvalidFormatException{
-			if (input == null || !(input instanceof String) || ((String)input).length() != 13){
-				return "";
-			}
-			String inputStr = (String)input;
-			inputStr = inputStr.substring(1,4)+inputStr.substring(5,8)+inputStr.substring(9,13);
-			return inputStr;
-		}
-	}
+	}	
 
 	private void swapLabel()
 	{
--- branches/4.0.0/CruxShowcase/src/org/cruxframework/crux/showcase/client/controller/ParameterBindController.java
+++ branches/4.0.0/CruxShowcase/src/org/cruxframework/crux/showcase/client/controller/ParameterBindController.java
@@ -1,54 +1,26 @@
 package org.cruxframework.crux.showcase.client.controller;
 
-import org.cruxframework.crux.core.client.controller.Controller;
-import org.cruxframework.crux.core.client.controller.Create;
-import org.cruxframework.crux.core.client.controller.Expose;
-import org.cruxframework.crux.core.client.controller.Parameter;
-import org.cruxframework.crux.core.client.controller.ParameterObject;
+import java.util.Date;
 
-import com.google.gwt.user.client.Window;
+import org.cruxframework.crux.core.client.controller.Controller;
+import org.cruxframework.crux.core.client.controller.Expose;
+import org.cruxframework.crux.widgets.client.dialog.Popup;
+
+import com.google.gwt.http.client.URL;
 
 @Controller("parameterBindController")
 public class ParameterBindController {
 	
-	@Create
-	protected Parameters parameters;
-
-	@Parameter(required=true)
-	protected String parameter;
-	
 	@Expose
 	public void onClick(){
-		Window.alert("Parameter: "+parameter);
-		Window.alert("Parameter (from parameter object): "+parameters.getParameter());
-	}
-	
-	@ParameterObject
-	public static class Parameters
-	{
-		@Parameter(required=true)
-		private String parameter;
 		
-		private Integer intParamenter;
-
-		public Integer getIntParamenter()
-		{
-			return intParamenter;
-		}
-
-		public void setIntParamenter(Integer intParamenter)
-		{
-			this.intParamenter = intParamenter;
-		}
-
-		public String getParameter()
-		{
-			return parameter;
-		}
-
-		public void setParameter(String parameter)
-		{
-			this.parameter = parameter;
-		}
+		String parameters = 
+					"?name=" + URL.encodeQueryString("Belle Lee Button") 
+					+ "&phone=" + 1234567890L 
+					+ "&dateOfBirth=" + new Date().getTime();
+		
+		String url = "parameterBindPopup.html" + parameters;
+		
+		Popup.show("Parameter Bind", url, "800", "350", null, null, true, true);
 	}
 }
