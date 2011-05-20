c621f677517eeb5270f1dfbcfc166ff9d2dcd5db
gesse@cruxframework.org <gesse@cruxframework.org@a5d2bbaa-053c-11de-b17c-0f1ef23b492c>
2011-05-20 23:44:13 +0000

    ENHANCEMENT - Issue #235 Issue #133
    git-svn-id: http://crux-framework.googlecode.com/svn@2868 a5d2bbaa-053c-11de-b17c-0f1ef23b492c

--- branches/4.0.0/CruxShowcase/src/org/cruxframework/crux/showcase/client/controller/ParameterBindSourcesController.java
+++ branches/4.0.0/CruxShowcase/src/org/cruxframework/crux/showcase/client/controller/ParameterBindSourcesController.java
@@ -3,7 +3,6 @@ package org.cruxframework.crux.showcase.client.controller;
 import java.util.ArrayList;
 
 import org.cruxframework.crux.core.client.controller.Controller;
-import org.cruxframework.crux.core.client.screen.JSWindow;
 
 @Controller("parameterBindSourcesController")
 public class ParameterBindSourcesController extends BaseSourcesController implements ParameterBindSourcesControllerCrossDoc {
@@ -14,7 +13,7 @@ public class ParameterBindSourcesController extends BaseSourcesController implem
 	protected ArrayList<SourceTab> getAdditionalSources() {
 		ArrayList<SourceTab> aditionalTabs = new ArrayList<SourceTab>();
 		aditionalTabs.add(new SourceTab("client/dto/SimpleContact.java", "Parameter DTO Source", true));
-		aditionalTabs.add(new SourceTab("parameterBindPopup.crux.xml", "Popup  Formatter", true));
+		aditionalTabs.add(new SourceTab("parameterBindPopup.crux.xml", "Popup Page Source", false));
 		aditionalTabs.add(new SourceTab("client/controller/ParameterBindPopupController.java", "Popup Controller Source", true));
 		return aditionalTabs;
 	}
