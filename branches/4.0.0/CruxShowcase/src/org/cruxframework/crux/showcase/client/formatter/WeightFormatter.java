ecf72514a256c39500f7561dafa3b4b84872f67a
thiago@cruxframework.org <thiago@cruxframework.org@a5d2bbaa-053c-11de-b17c-0f1ef23b492c>
2011-05-25 21:22:53 +0000

    IGNORE - Showcase formatter
    git-svn-id: http://crux-framework.googlecode.com/svn@2884 a5d2bbaa-053c-11de-b17c-0f1ef23b492c

--- branches/4.0.0/CruxShowcase/src/org/cruxframework/crux/showcase/client/formatter/WeightFormatter.java
+++ branches/4.0.0/CruxShowcase/src/org/cruxframework/crux/showcase/client/formatter/WeightFormatter.java
@@ -8,16 +8,18 @@ import com.google.gwt.i18n.client.NumberFormat;
 @FormatterName("weight")
 public class WeightFormatter implements Formatter {
 
+	private static final String LBS = " lbs.";
+
 	/**
 	 * @see org.cruxframework.crux.core.client.formatter.Formatter#unformat(java.lang.String)
 	 */
 	public Object unformat(String input) {
 		
-		if (input == null || !input.endsWith(" lbs.")) {
+		if (input == null || !input.endsWith(LBS)) {
 			return null;
 		}
 
-		return Integer.parseInt(input.substring(0, input.length() - 4));
+		return Integer.parseInt(input.substring(0, input.length() - LBS.length()));
 	}
 
 	/**
@@ -33,6 +35,6 @@ public class WeightFormatter implements Formatter {
 			throw new InvalidFormatException();
 		}
 
-		return NumberFormat.getFormat("###").format((Integer) input)  + " lbs.";
+		return NumberFormat.getFormat("###").format((Integer) input)  + LBS;
 	}
 }
