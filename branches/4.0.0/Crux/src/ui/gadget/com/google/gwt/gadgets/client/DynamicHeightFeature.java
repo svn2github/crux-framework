3440a3849ac328adee5290a890fa11f714e72342
thiago@cruxframework.org <thiago@cruxframework.org@a5d2bbaa-053c-11de-b17c-0f1ef23b492c>
2011-06-15 16:37:48 +0000

    ENHANCEMENT - Issue #159 - Add support for dynamic-height feature
    git-svn-id: http://crux-framework.googlecode.com/svn@2917 a5d2bbaa-053c-11de-b17c-0f1ef23b492c

--- branches/4.0.0/Crux/src/ui/gadget/com/google/gwt/gadgets/client/DynamicHeightFeature.java
+++ branches/4.0.0/Crux/src/ui/gadget/com/google/gwt/gadgets/client/DynamicHeightFeature.java
@@ -1,12 +1,12 @@
 /*
- * Copyright 2010 Google Inc.
- *
+ * Copyright 2011 cruxframework.org.
+ * 
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
- *
+ * 
  * http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -15,7 +15,6 @@
  */
 package com.google.gwt.gadgets.client;
 
-import com.google.gwt.user.client.ui.RootPanel;
 
 /**
  * Provides access to the dynamic height feature.
@@ -23,17 +22,19 @@ import com.google.gwt.user.client.ui.RootPanel;
 public interface DynamicHeightFeature {
 
   /**
-   * Causes the Gadget to be resized.  Make sure you have added your
-   * content to the RootPanel returned by {@link #getContentDiv()}.
+   * Causes the Gadget to be resized. Will attempt to fit the gadget to its content 
    */
   void adjustHeight();
 
   /**
-   * Use this method to retrieve the RootPanel to attach all gadget content to
-   * when using DynamicHeight.
-   *
-   * @return a pointer to a panel that will allow your gadget to be resizable
-   *         when you add content here.
+   * Causes the Gadget to be resized. 
+   * @param height A preferred height in pixels. 
    */
-  RootPanel getContentDiv();
+  void adjustHeight(int height);
+  
+  /**
+   * Detects the inner dimensions of a frame. See http://www.quirksmode.org/dom/w3c_cssom.html for more information.
+   * @returnAn object with width and height properties.
+   */
+  ViewPortDimensions getViewportDimensions();
 }
--- branches/4.0.0/Crux/src/ui/gadget/com/google/gwt/gadgets/client/impl/DynamicHeightFeatureImpl.java
+++ branches/4.0.0/Crux/src/ui/gadget/com/google/gwt/gadgets/client/impl/DynamicHeightFeatureImpl.java
@@ -1,12 +1,12 @@
 /*
- * Copyright 2010 Google Inc.
- *
+ * Copyright 2011 cruxframework.org.
+ * 
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
- *
+ * 
  * http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -16,20 +16,25 @@
 package com.google.gwt.gadgets.client.impl;
 
 import com.google.gwt.gadgets.client.DynamicHeightFeature;
-import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.gadgets.client.ViewPortDimensions;
 
 /**
  * Provides access to the dynamic height feature.
  */
 public class DynamicHeightFeatureImpl implements DynamicHeightFeature {
-  private DynamicHeightFeatureImpl() {
-  }
+    private DynamicHeightFeatureImpl() {
+    }
 
-  public native void adjustHeight() /*-{
-    $wnd.gadgets.window.adjustHeight();
-  }-*/;
+    public native void adjustHeight() /*-{
+       $wnd.gadgets.window.adjustHeight();
+    }-*/;
 
-  public RootPanel getContentDiv() {
-    return RootPanel.get("__gwt_gadget_content_div");
-  }
+    public native void adjustHeight(int height) /*-{
+	    $wnd.gadgets.window.adjustHeight(height);
+	 }-*/;
+
+    public native ViewPortDimensions getViewportDimensions()/*-{
+        $wnd.gadgets.window.getViewportDimensions();
+	}-*/;
+
 }
