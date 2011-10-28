a5c98447890347ddd1ecb7e6a1a37e66bdab0e73
thiago@cruxframework.org <thiago@cruxframework.org@a5d2bbaa-053c-11de-b17c-0f1ef23b492c>
2011-10-28 14:02:58 +0000

    git-svn-id: http://crux-framework.googlecode.com/svn@3063 a5d2bbaa-053c-11de-b17c-0f1ef23b492c

--- branches/4.0.0/CruxSite/src/org/cruxframework/cruxsite/client/remote/GreetingServiceAsync.java
+++
@@ -1,8 +0,0 @@
-package org.cruxframework.cruxsite.client.remote;
-
-import com.google.gwt.user.client.rpc.AsyncCallback;
-
-public interface GreetingServiceAsync
-{
-	public void getHelloMessage(String name, AsyncCallback<String> callback);
-}
