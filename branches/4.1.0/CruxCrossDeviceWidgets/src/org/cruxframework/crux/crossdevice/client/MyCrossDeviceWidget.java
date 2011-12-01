da6bcd9d51c5e6dbe69a4a531bb559cd261fca6e
thiago@cruxframework.org <thiago@cruxframework.org@a5d2bbaa-053c-11de-b17c-0f1ef23b492c>
2011-12-01 12:07:41 +0000

    git-svn-id: http://crux-framework.googlecode.com/svn@3131 a5d2bbaa-053c-11de-b17c-0f1ef23b492c

---
+++ branches/4.1.0/CruxCrossDeviceWidgets/src/org/cruxframework/crux/crossdevice/client/MyCrossDeviceWidget.java
@@ -0,0 +1,15 @@
+package org.cruxframework.crux.crossdevice.client;
+
+import org.cruxframework.crux.core.client.screen.DeviceAdaptive;
+import org.cruxframework.crux.core.client.screen.DeviceAdaptive.Device;
+import org.cruxframework.crux.core.client.screen.DeviceAdaptive.Template;
+import org.cruxframework.crux.core.client.screen.DeviceAdaptive.Templates;
+
+@Templates({
+	@Template(name="myCrossDeviceAll", device=Device.all),
+	@Template(name="myCrossDeviceAndroids", device=Device.androids)
+})
+public interface MyCrossDeviceWidget extends DeviceAdaptive
+{
+
+}
