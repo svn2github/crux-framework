d699a919a0210184b53eea4d042160fdbf0d9848
daniel@cruxframework.org <daniel@cruxframework.org@a5d2bbaa-053c-11de-b17c-0f1ef23b492c>
2012-03-23 19:50:19 +0000

    ENHANCEMENT: Issue #273
    git-svn-id: http://crux-framework.googlecode.com/svn@3219 a5d2bbaa-053c-11de-b17c-0f1ef23b492c

--- branches/4.1.0/Crux/src/ui/widgets/org/cruxframework/crux/widgets/rebind/listbox/ValueListBoxFactory.java
+++ branches/4.1.0/Crux/src/ui/widgets/org/cruxframework/crux/widgets/rebind/listbox/ValueListBoxFactory.java
@@ -15,8 +15,19 @@
  */
 package org.cruxframework.crux.widgets.rebind.listbox;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.cruxframework.crux.core.client.dto.DataObjectLabel;
+import org.cruxframework.crux.core.i18n.MessagesFactory;
 import org.cruxframework.crux.core.rebind.CruxGeneratorException;
+import org.cruxframework.crux.core.rebind.GeneratorMessages;
 import org.cruxframework.crux.core.rebind.dto.DataObjects;
+import org.cruxframework.crux.core.rebind.dto.DataObjects.Label;
 import org.cruxframework.crux.core.rebind.screen.widget.ViewFactoryCreator.SourcePrinter;
 import org.cruxframework.crux.core.rebind.screen.widget.WidgetCreator;
 import org.cruxframework.crux.core.rebind.screen.widget.WidgetCreatorContext;
@@ -30,9 +41,14 @@ import org.cruxframework.crux.core.rebind.screen.widget.creator.HasDirectionEsti
 import org.cruxframework.crux.core.rebind.screen.widget.creator.HasDoubleClickHandlersFactory;
 import org.cruxframework.crux.core.rebind.screen.widget.creator.HasEnabledFactory;
 import org.cruxframework.crux.core.rebind.screen.widget.creator.HasNameFactory;
+import org.cruxframework.crux.core.rebind.screen.widget.creator.children.WidgetChildProcessor;
 import org.cruxframework.crux.core.rebind.screen.widget.declarative.DeclarativeFactory;
 import org.cruxframework.crux.core.rebind.screen.widget.declarative.TagAttributeDeclaration;
 import org.cruxframework.crux.core.rebind.screen.widget.declarative.TagAttributesDeclaration;
+import org.cruxframework.crux.core.rebind.screen.widget.declarative.TagChild;
+import org.cruxframework.crux.core.rebind.screen.widget.declarative.TagChildren;
+import org.cruxframework.crux.core.rebind.screen.widget.declarative.TagConstraints;
+import org.cruxframework.crux.core.utils.ClassUtils;
 import org.cruxframework.crux.core.utils.ProvidesKeyUtils;
 import org.cruxframework.crux.core.utils.RendererUtils;
 import org.cruxframework.crux.widgets.client.listbox.ValueListBox;
@@ -44,7 +60,9 @@ import org.cruxframework.crux.widgets.client.listbox.ValueListBox;
  *
  */
 @DeclarativeFactory(id="valueListBox", library="widgets", targetWidget=ValueListBox.class)
-
+@TagChildren({
+	@TagChild(ValueListBoxFactory.ValueListBoxItemsProcessor.class)
+})
 @TagAttributesDeclaration({
 	@TagAttributeDeclaration(value="dataObject", required=true)
 
@@ -55,8 +73,107 @@ public class ValueListBoxFactory extends WidgetCreator<WidgetCreatorContext> imp
 				HasEnabledFactory<WidgetCreatorContext>, HasAllFocusHandlersFactory<WidgetCreatorContext>, HasAllKeyHandlersFactory<WidgetCreatorContext>,
 				HasAllMouseHandlersFactory<WidgetCreatorContext>
 {
-	@Override
-	public void processChildren(SourcePrinter out, WidgetCreatorContext context) throws CruxGeneratorException {}
+
+	@TagConstraints(tagName="item", minOccurs="0", maxOccurs="unbounded")
+	@TagAttributesDeclaration({
+		@TagAttributeDeclaration("value"),
+		@TagAttributeDeclaration(value="label", supportsI18N=true),
+		@TagAttributeDeclaration(value="selected", type=Boolean.class)
+	})
+	public static class ValueListBoxItemsProcessor extends WidgetChildProcessor<WidgetCreatorContext>
+	{
+		private static final Log logger = LogFactory.getLog(ValueListBoxItemsProcessor.class);
+		protected static GeneratorMessages messages = (GeneratorMessages)MessagesFactory.getMessages(GeneratorMessages.class);
+
+		@Override
+		public void processChildren(SourcePrinter out, WidgetCreatorContext context) throws CruxGeneratorException
+		{
+			try
+			{
+				String dataObject = context.readWidgetProperty("dataObject");
+				String dataObjectClass = DataObjects.getDataObject(dataObject);
+				Class<?> dataClass = Class.forName(dataObjectClass);
+				List<Label> labels = extractValueItem(dataClass);
+
+				String label = context.readChildProperty("label");
+				String value = context.readChildProperty("value");
+
+				if (value == null || value.length() == 0)
+				{
+					value = label;
+				}
+
+				String variableName = getWidgetCreator().createVariableName("object");
+				out.println(dataObjectClass + " " + variableName + " = new " + dataObjectClass +"();");
+
+				int beginIndex = 0;
+				int endIndex = 0;
+
+				for (Label labelObject : labels)
+		        {
+					if(!"".equals(labelObject.getSuffix()) &&  value.indexOf(labelObject.getSuffix()) != -1)
+					{
+						endIndex = value.indexOf(labelObject.getSuffix());
+					}
+					else
+					{
+						endIndex = value.length();
+					}
+
+					out.println(variableName + "." +labelObject.getLabelField() + "(\"" + value.substring(beginIndex, endIndex)  + "\");");
+					beginIndex = endIndex;
+					endIndex = value.indexOf(labelObject.getSuffix(), beginIndex);
+		        }
+
+				out.println(context.getWidget()+".setValue(" + variableName +");");
+			}
+			catch (ClassNotFoundException e)
+			{
+				logger.error(messages.dataObjectsInitializeError(e.getLocalizedMessage()),e);
+			}
+
+		}
+
+
+		private List<Label> extractValueItem(Class<?> dataClass)
+		{
+			List<Label> labels = new ArrayList<Label>();
+
+			while(dataClass.getSuperclass() != null)
+			{
+				Field[] fields = dataClass.getDeclaredFields();
+
+				for (Field field : fields)
+				{
+					DataObjectLabel annotation = field.getAnnotation(DataObjectLabel.class);
+
+					if (annotation != null)
+					{
+						if (Modifier.isPublic(field.getModifiers()))
+						{
+							Label label = new Label();
+							label.setLabelField(field.getName());
+							label.setSuffix(annotation.separator());
+
+							labels.add(label);
+						}
+						else
+						{
+							Label label = new Label();
+							label.setLabelField(ClassUtils.getSetterMethod(field.getName()));
+							label.setSuffix(annotation.separator());
+
+							labels.add(label);
+						}
+					}
+				}
+				dataClass = dataClass.getSuperclass();
+			}
+
+			return labels;
+		}
+
+	}
 
 	@Override
 	public void instantiateWidget(SourcePrinter out, WidgetCreatorContext context)
