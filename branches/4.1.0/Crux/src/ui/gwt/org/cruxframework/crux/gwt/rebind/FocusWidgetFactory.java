7fa57f2a26497cb0f606a59bc05ff0294578cd2f
daniel@cruxframework.org <daniel@cruxframework.org@a5d2bbaa-053c-11de-b17c-0f1ef23b492c>
2012-04-27 16:09:19 +0000

    Refactor of the FocusWidgetFactory for using the specified interfaces
    git-svn-id: http://crux-framework.googlecode.com/svn@3263 a5d2bbaa-053c-11de-b17c-0f1ef23b492c

--- branches/4.1.0/Crux/src/ui/gwt/org/cruxframework/crux/gwt/rebind/FocusWidgetFactory.java
+++ branches/4.1.0/Crux/src/ui/gwt/org/cruxframework/crux/gwt/rebind/FocusWidgetFactory.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2011 cruxframework.org.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -18,27 +18,19 @@ package org.cruxframework.crux.gwt.rebind;
 import org.cruxframework.crux.core.rebind.screen.widget.WidgetCreator;
 import org.cruxframework.crux.core.rebind.screen.widget.WidgetCreatorContext;
 import org.cruxframework.crux.core.rebind.screen.widget.creator.FocusableFactory;
-import org.cruxframework.crux.core.rebind.screen.widget.creator.event.BlurEvtBind;
+import org.cruxframework.crux.core.rebind.screen.widget.creator.HasAllFocusHandlersFactory;
+import org.cruxframework.crux.core.rebind.screen.widget.creator.HasAllKeyHandlersFactory;
+import org.cruxframework.crux.core.rebind.screen.widget.creator.HasAllMouseHandlersFactory;
 import org.cruxframework.crux.core.rebind.screen.widget.creator.event.ClickEvtBind;
 import org.cruxframework.crux.core.rebind.screen.widget.creator.event.DoubleClickEvtBind;
-import org.cruxframework.crux.core.rebind.screen.widget.creator.event.FocusEvtBind;
-import org.cruxframework.crux.core.rebind.screen.widget.creator.event.KeyDownEvtBind;
-import org.cruxframework.crux.core.rebind.screen.widget.creator.event.KeyPressEvtBind;
-import org.cruxframework.crux.core.rebind.screen.widget.creator.event.KeyUpEvtBind;
-import org.cruxframework.crux.core.rebind.screen.widget.creator.event.MouseDownEvtBind;
-import org.cruxframework.crux.core.rebind.screen.widget.creator.event.MouseMoveEvtBind;
-import org.cruxframework.crux.core.rebind.screen.widget.creator.event.MouseOutEvtBind;
-import org.cruxframework.crux.core.rebind.screen.widget.creator.event.MouseOverEvtBind;
-import org.cruxframework.crux.core.rebind.screen.widget.creator.event.MouseUpEvtBind;
-import org.cruxframework.crux.core.rebind.screen.widget.creator.event.MouseWheelEvtBind;
 import org.cruxframework.crux.core.rebind.screen.widget.declarative.TagAttribute;
 import org.cruxframework.crux.core.rebind.screen.widget.declarative.TagAttributes;
 import org.cruxframework.crux.core.rebind.screen.widget.declarative.TagEvent;
 import org.cruxframework.crux.core.rebind.screen.widget.declarative.TagEvents;
 
 /**
- * This is the base factory class for widgets that can receive focus. 
- * 
+ * This is the base factory class for widgets that can receive focus.
+ *
  * @author Thiago Bustamante
  *
  */
@@ -47,19 +39,8 @@ import org.cruxframework.crux.core.rebind.screen.widget.declarative.TagEvents;
 })
 @TagEvents({
 	@TagEvent(ClickEvtBind.class),
-	@TagEvent(DoubleClickEvtBind.class),
-	@TagEvent(FocusEvtBind.class),
-	@TagEvent(BlurEvtBind.class),
-	@TagEvent(KeyPressEvtBind.class),
-	@TagEvent(KeyUpEvtBind.class),
-	@TagEvent(KeyDownEvtBind.class),
-	@TagEvent(MouseDownEvtBind.class),
-	@TagEvent(MouseUpEvtBind.class),
-	@TagEvent(MouseOverEvtBind.class),
-	@TagEvent(MouseOutEvtBind.class),
-	@TagEvent(MouseMoveEvtBind.class),
-	@TagEvent(MouseWheelEvtBind.class)
+	@TagEvent(DoubleClickEvtBind.class)
 })
-public abstract class FocusWidgetFactory <C extends WidgetCreatorContext> extends WidgetCreator<C> implements FocusableFactory<C> 
+public abstract class FocusWidgetFactory <C extends WidgetCreatorContext> extends WidgetCreator<C> implements FocusableFactory<C>, HasAllMouseHandlersFactory<C>, HasAllKeyHandlersFactory<C>, HasAllFocusHandlersFactory<C>
 {
 }
