342d8fc45f55b3e071d485c97e3a47f3916f0048
thiago@cruxframework.org <thiago@cruxframework.org@a5d2bbaa-053c-11de-b17c-0f1ef23b492c>
2012-09-14 21:41:38 +0000

    ENHANCEMENT - View management (support regular expressions on useView attribute)
    git-svn-id: http://crux-framework.googlecode.com/svn@3405 a5d2bbaa-053c-11de-b17c-0f1ef23b492c

--- branches/4.1.0/Crux/src/core/org/cruxframework/crux/core/declarativeui/view/Views.java
+++ branches/4.1.0/Crux/src/core/org/cruxframework/crux/core/declarativeui/view/Views.java
@@ -15,6 +15,7 @@
  */
 package org.cruxframework.crux.core.declarativeui.view;
  
+import java.io.File;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -31,6 +32,7 @@ import org.cruxframework.crux.core.rebind.module.Module;
 import org.cruxframework.crux.core.rebind.module.Modules;
 import org.cruxframework.crux.core.server.classpath.ClassPathResolverInitializer;
 import org.cruxframework.crux.core.utils.FilePatternHandler;
+import org.cruxframework.crux.core.utils.FileUtils;
 
 
 /**
@@ -190,13 +192,14 @@ public class Views
     }
 	
 	private static void extractFolderViews(List<String> result, URL baseFolder, FilePatternHandler handler)
-	{
+	{//TODO modificar a inicializacao dos scanners para nao procurar no classes nem no /war/moduleoutput
+	//TODO modificar o scanner de templates para identificar duplicatas....assim como o de views	
 		for (String viewId : views.keySet())
         {
 			URL view = views.get(viewId);
 	        if (view.toString().startsWith(baseFolder.toString()))
 	        {
-	        	String relativePath = view.toString().substring(baseFolder.toString().length()+1);
+	        	String relativePath = view.toString().substring(baseFolder.toString().length());
 	        	if (handler.isValidEntry(relativePath))
 	        	{
 	        		result.add(viewId);
@@ -224,9 +227,60 @@ public class Views
 	{
 		if (views.containsKey(viewId))
 		{
-			throw new ViewException("Duplicated view found. View: ["+viewId+"].");
+			checkIdentity(views.get(viewId), view, viewId);
 		}
-		
-		views.put(viewId, view);
+		else
+		{
+			views.put(viewId, view);
+		}
+	}
+	
+	private static void checkIdentity(URL view1, URL view2, String viewId)
+	{
+		if (!view1.toString().equals(view2.toString()))
+		{
+			if (view1.getProtocol().equals(view2.getProtocol()) && view1.getProtocol().equals("file"))
+			{
+				File file1;
+				File file2;
+				try
+                {
+	                file1 = new File(view1.toURI());
+	                file2 = new File(view2.toURI());
+                }
+                catch (Exception e)
+                {
+    				throw new ViewException("Invalid url for view ["+viewId+"].");
+                }
+	                
+                if (file1.length() == file2.length())
+                {
+					String view1Content;
+					String view2Content;
+    				try
+                    {
+    					view1Content = FileUtils.read(file1);
+    					view2Content = FileUtils.read(file1);
+                    }
+                    catch (Exception e)
+                    {
+        				throw new ViewException("Can not read the view file. View ID["+viewId+"].");
+                    }
+
+                	if (!view1Content.equals(view2Content))
+                	{
+                		throw new ViewException("Duplicated view identifier. View ["+viewId+"] is already registered.");
+                	}
+                }
+                else
+                {
+                	throw new ViewException("Duplicated view identifier. View ["+viewId+"] is already registered.");
+                }
+			}
+			else
+			{
+				throw new ViewException("Duplicated view identifier. View ["+viewId+"] is already registered.");
+			}
+		}
 	}
 }
