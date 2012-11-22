2899896f98f345e6744a2c85b3ae414170da125c
thiago@cruxframework.org <thiago@cruxframework.org@a5d2bbaa-053c-11de-b17c-0f1ef23b492c>
2012-11-22 15:39:19 +0000

    git-svn-id: http://crux-framework.googlecode.com/svn@3440 a5d2bbaa-053c-11de-b17c-0f1ef23b492c

--- branches/4.1.0/CruxSite/src/org/cruxframework/cruxsite/rebind/PromoBannerFactory.java
+++ branches/4.1.0/CruxSite/src/org/cruxframework/cruxsite/rebind/PromoBannerFactory.java
@@ -17,9 +17,9 @@ package org.cruxframework.cruxsite.rebind;
 
 import org.cruxframework.crux.core.client.utils.EscapeUtils;
 import org.cruxframework.crux.core.client.utils.StringUtils;
+import org.cruxframework.crux.core.rebind.AbstractProxyCreator.SourcePrinter;
 import org.cruxframework.crux.core.rebind.CruxGeneratorException;
 import org.cruxframework.crux.core.rebind.screen.widget.EvtProcessor;
-import org.cruxframework.crux.core.rebind.screen.widget.ViewFactoryCreator.SourcePrinter;
 import org.cruxframework.crux.core.rebind.screen.widget.WidgetCreator;
 import org.cruxframework.crux.core.rebind.screen.widget.WidgetCreatorContext;
 import org.cruxframework.crux.core.rebind.screen.widget.creator.children.WidgetChildProcessor;
