07aa33198746a6801fe6c9e71b33257221c621e5
thiago@cruxframework.org <thiago@cruxframework.org@a5d2bbaa-053c-11de-b17c-0f1ef23b492c>
2013-06-22 01:10:31 +0000

    git-svn-id: http://crux-framework.googlecode.com/svn@3606 a5d2bbaa-053c-11de-b17c-0f1ef23b492c

--- branches/4.1.0/Crux/src/core/org/cruxframework/crux/core/rebind/rest/CruxRestProxyCreator.java
+++ branches/4.1.0/Crux/src/core/org/cruxframework/crux/core/rebind/rest/CruxRestProxyCreator.java
@@ -98,12 +98,6 @@ public class CruxRestProxyCreator extends AbstractInterfaceWrapperProxyCreator
 	public CruxRestProxyCreator(TreeLogger logger, GeneratorContextExt context, JClassType baseIntf)
 	{
 		super(logger, context, baseIntf, true);
-		if(!cacheableVersionFound()) {
-			initializeInnerMethods();
-		}
-	}
-
-	private void initializeInnerMethods() {
 		callbackType = context.getTypeOracle().findType(Callback.class.getCanonicalName());
 		javascriptObjectType = context.getTypeOracle().findType(JavaScriptObject.class.getCanonicalName());
 		restImplementationClass = getRestImplementationClass(baseIntf);
