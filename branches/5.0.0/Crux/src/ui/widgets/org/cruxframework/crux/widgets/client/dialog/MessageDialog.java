57f7ea828eee326e4b8b20b8157f7c5ee63ec154
samuel@cruxframework.org <samuel@cruxframework.org@a5d2bbaa-053c-11de-b17c-0f1ef23b492c>
2013-09-24 15:46:48 +0000

    ENHANCEMENT adding client side handler to process a file
    git-svn-id: http://crux-framework.googlecode.com/svn@3961 a5d2bbaa-053c-11de-b17c-0f1ef23b492c

--- branches/5.0.0/Crux/src/ui/widgets/org/cruxframework/crux/widgets/client/dialog/MessageDialog.java
+++ branches/5.0.0/Crux/src/ui/widgets/org/cruxframework/crux/widgets/client/dialog/MessageDialog.java
@@ -79,8 +79,6 @@ public class MessageDialog implements HasOkHandlers, HasAnimation, IsWidget, Ori
 		messagePanel.getElement().getParentElement().setAttribute("align", "center");
 
 		setStyleName(DEFAULT_STYLE_NAME);
-		
-		handleOrientationChangeHandlers();
     }
 
 	private void handleOrientationChangeHandlers() {
@@ -230,6 +228,7 @@ public class MessageDialog implements HasOkHandlers, HasAnimation, IsWidget, Ori
 		try
 		{
 			Screen.blockToUser("crux-MessageDialogScreenBlocker");
+			handleOrientationChangeHandlers();
 			openedDialogBoxes.add(dialogBox);
 			dialogBox.center();
 			dialogBox.show();
