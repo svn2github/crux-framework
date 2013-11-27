6ca65db68c5ad36d5912bc5dea3a746d7e40305a
thiago@cruxframework.org <thiago@cruxframework.org@a5d2bbaa-053c-11de-b17c-0f1ef23b492c>
2013-11-27 20:59:12 +0000

    new database example on showcase
    git-svn-id: http://crux-framework.googlecode.com/svn@4228 a5d2bbaa-053c-11de-b17c-0f1ef23b492c

---
+++ branches/5.0.0/cross-device-showcase/src/main/java/org/cruxframework/crossdeviceshowcase/client/controller/MainController.java
@@ -0,0 +1,17 @@
+package org.cruxframework.crossdeviceshowcase.client.controller;
+
+import org.cruxframework.crux.core.client.controller.Controller;
+import org.cruxframework.crux.core.client.controller.Expose;
+import org.cruxframework.crux.core.client.screen.Screen;
+import org.cruxframework.crux.widgets.client.disposal.menutabsdisposal.MenuTabsDisposal;
+
+@Controller("mainController")
+public class MainController 
+{
+	@Expose
+	public void showMenu()
+	{
+		MenuTabsDisposal menuDisposal = (MenuTabsDisposal) Screen.get("menuDisposal");
+		menuDisposal.showMenu();
+	}
+}
