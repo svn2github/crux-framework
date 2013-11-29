f1e59de5a8f749f14e79018503920d6e0c7dc40f
samuel@cruxframework.org <samuel@cruxframework.org@a5d2bbaa-053c-11de-b17c-0f1ef23b492c>
2013-11-29 22:11:14 +0000

    ENHANCEMENT adding ListShuttle widget
    git-svn-id: http://crux-framework.googlecode.com/svn@4244 a5d2bbaa-053c-11de-b17c-0f1ef23b492c

--- branches/5.0.0/Crux/src/ui/widgets/org/cruxframework/crux/widgets/client/listshuttle/ListShuttle.java
+++ branches/5.0.0/Crux/src/ui/widgets/org/cruxframework/crux/widgets/client/listshuttle/ListShuttle.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2013 cruxframework.org.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
 package org.cruxframework.crux.widgets.client.listshuttle;
 
 import java.util.ArrayList;
@@ -132,23 +147,6 @@ public class ListShuttle<T> extends Composite implements IListShuttle<T>
 				}
 
 				String value = beanRenderer.render(data);
-
-				/*
-				// Add a DragStartHandler.
-				final Label w = new Label(value);
-				w.getElement().setDraggable(Element.DRAGGABLE_TRUE);
-				
-				w.addDragStartHandler(new DragStartHandler() {
-				  @Override
-				  public void onDragStart(DragStartEvent event) {
-				    // Required: set data for the event.
-				    event.setData("text", "Hello World");
-				    // Optional: show a copy of the widget under cursor.
-				    event.getDataTransfer().setDragImage(w.getElement(), 10, 10);
-				  }
-				});
-				*/
-				
 				sb.append(SafeHtmlUtils.fromString(value));
 			}
 		}
