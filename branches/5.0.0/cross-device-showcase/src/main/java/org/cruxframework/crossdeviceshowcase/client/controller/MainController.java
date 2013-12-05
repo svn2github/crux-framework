5dd8cafaed8dbb622dba5c44bf110138550fb5b8
gesse@cruxframework.org <gesse@cruxframework.org@a5d2bbaa-053c-11de-b17c-0f1ef23b492c>
2013-12-05 20:36:57 +0000

    IGNORE: showcase - showing sources from SVN, adjusting styles
    git-svn-id: http://crux-framework.googlecode.com/svn@4282 a5d2bbaa-053c-11de-b17c-0f1ef23b492c

--- branches/5.0.0/cross-device-showcase/src/main/java/org/cruxframework/crossdeviceshowcase/client/controller/MainController.java
+++ branches/5.0.0/cross-device-showcase/src/main/java/org/cruxframework/crossdeviceshowcase/client/controller/MainController.java
@@ -1,17 +1,40 @@
 package org.cruxframework.crossdeviceshowcase.client.controller;
 
+import org.cruxframework.crossdeviceshowcase.client.remote.SVNServiceAsync;
 import org.cruxframework.crux.core.client.controller.Controller;
 import org.cruxframework.crux.core.client.controller.Expose;
+import org.cruxframework.crux.core.client.ioc.Inject;
+import org.cruxframework.crux.core.client.rpc.AsyncCallbackAdapter;
 import org.cruxframework.crux.core.client.screen.Screen;
 import org.cruxframework.crux.widgets.client.disposal.menutabsdisposal.MenuTabsDisposal;
+
+import com.google.gwt.user.client.Window;
 
 @Controller("mainController")
 public class MainController 
 {
+	@Inject
+	public SVNServiceAsync service;
+	
 	@Expose
 	public void showMenu()
 	{
 		MenuTabsDisposal menuDisposal = (MenuTabsDisposal) Screen.get("menuDisposal");
 		menuDisposal.showMenu();
+	}
+	
+	@Expose
+	public void exibirCodigoFonte()
+	{
+		MenuTabsDisposal menuDisposal = (MenuTabsDisposal) Screen.get("menuDisposal");
+		String viewId = menuDisposal.getCurrentView();
+		service.getXmlFile(viewId + ".view.xml", true, new AsyncCallbackAdapter<String>() 
+		{
+			@Override
+			public void onComplete(String result) 
+			{
+				Window.alert(result);
+			}
+		});
 	}
 }
