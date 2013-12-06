24ce8febc9c766ead7ca4f067e9417146d8dc6e9
thiago@cruxframework.org <thiago@cruxframework.org@a5d2bbaa-053c-11de-b17c-0f1ef23b492c>
2013-12-06 12:24:17 +0000

    Javadoc for widget
    git-svn-id: http://crux-framework.googlecode.com/svn@4287 a5d2bbaa-053c-11de-b17c-0f1ef23b492c

--- branches/5.0.0/Crux/src/ui/widgets/org/cruxframework/crux/widgets/client/dialogcontainer/DialogViewContainer.java
+++ branches/5.0.0/Crux/src/ui/widgets/org/cruxframework/crux/widgets/client/dialogcontainer/DialogViewContainer.java
@@ -29,8 +29,8 @@ import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.Panel;
 
 /**
+ * A View Container that render its views inside a floating dialog box.
  * @author Thiago da Rosa de Bustamante
- *
  */
 public class DialogViewContainer extends SingleViewContainer 
 {
@@ -40,33 +40,80 @@ public class DialogViewContainer extends SingleViewContainer
 	private View innerView;
 	private boolean unloadViewOnClose;
 	
+	/**
+	 * Create a dialog container, load the given view into the container and open the dialog box. 
+	 * @param viewName name of the view to be opened
+	 */
 	public static void openDialog(String viewName)
 	{
 		DialogViewContainer container = createDialog(viewName, viewName, true);
 		container.openDialog();
 	}
-	
+
+	/**
+	 * Create a dialog container, load the given view into the container and open the dialog box. 
+	 * @param viewName name of the view to be opened
+	 * @param viewId ID to be used to identify the opened view
+	 * @param closeable if true, a close button will be present at to top of dialog box
+	 */
 	public static void openDialog(String viewName, String viewId, boolean closeable)
 	{
 		DialogViewContainer container = createDialog(viewName, viewId, closeable);
 		container.openDialog();
 	}
 
+	/**
+	 * Create a new DialogViewContainer and load the given view into the container
+	 * @param viewName name of the view to be loaded
+	 * @return the container created
+	 */
 	public static DialogViewContainer createDialog(String viewName)
 	{
 		return createDialog(viewName, viewName, true);
 	}
 	
+	/**
+	 * Create a new DialogViewContainer and load the given view into the container.
+	 * @param viewName name of the view to be loaded
+	 * @param viewId ID to be used to identify the opened view
+	 * @param closeable if true, a close button will be present at to top of dialog box
+	 * @return the container created
+	 */
 	public static DialogViewContainer createDialog(String viewName, String viewId, boolean closeable)
 	{
 		return createDialog(viewName, viewName, true, true, null, null, -1, -1);
 	}
-	
+
+	/**
+	 * Create a new DialogViewContainer and load the given view into the container.
+	 * @param viewName name of the view to be loaded
+	 * @param viewId ID to be used to identify the opened view
+	 * @param closeable if true, a close button will be present at to top of dialog box
+	 * @param modal if true, a modal dialog will be created
+	 * @param width dialog box width
+	 * @param height dialog box height
+	 * @param left left position of the dialog box
+	 * @param top top position of the dialog box
+	 * @return the container created
+	 */
 	public static DialogViewContainer createDialog(String viewName, String viewId, boolean closeable, boolean modal, String width, String height, int left, int top)
 	{
 		return createDialog(viewName, viewId, closeable, modal, width, height, left, top, null);
 	}
 	
+	/**
+	 * Create a new DialogViewContainer and load the given view into the container.
+	 * @param viewName name of the view to be loaded
+	 * @param viewId ID to be used to identify the opened view
+	 * @param closeable if true, a close button will be present at to top of dialog box
+	 * @param modal if true, a modal dialog will be created
+	 * @param width dialog box width
+	 * @param height dialog box height
+	 * @param left left position of the dialog box
+	 * @param top top position of the dialog box
+	 * @param parameter any object you want to pass as parameter to the view's activate and load event
+	 * @return the container created
+	 */
 	public static DialogViewContainer createDialog(String viewName, String viewId, boolean closeable, boolean modal, String width, String height, int left, int top, Object parameter)
 	{
 		DialogViewContainer container = new DialogViewContainer(closeable, modal);
@@ -82,11 +129,20 @@ public class DialogViewContainer extends SingleViewContainer
 		return container;
 	}
 	
+	/**
+	 * Default Constructor. The created dialog will be closeable and modal.
+	 */
 	public DialogViewContainer()
 	{
 		this(true, true);
 	}
 	
+	/**
+	 * Constructor.
+	 * 
+	 * @param closeable if true, a close button will be present at to top of dialog box
+	 * @param modal if true, a modal dialog will be created
+	 */
 	public DialogViewContainer(boolean closeable, boolean modal)
 	{
 		super(null, true);
@@ -95,6 +151,8 @@ public class DialogViewContainer extends SingleViewContainer
 		
 		contentPanel = new FlowPanel();
 		contentPanel.setWidth("100%");
+		
+		unloadViewOnClose = true;
 		
 		FlowPanel bodyPanel = new FlowPanel();
 		bodyPanel.setWidth("100%");
@@ -124,43 +182,80 @@ public class DialogViewContainer extends SingleViewContainer
 		initWidget(new Label());
 	}
 
+	/**
+	 * Read the unloadViewOnClose property value. This property tells the container if it must unload the containing 
+	 * view when the dialog is closed. 
+	 * @return unloadViewOnClose property value
+	 */
 	public boolean isUnloadViewOnClose()
     {
     	return unloadViewOnClose;
     }
 
+	/**
+	 * Write the unloadViewOnClose property value. This property tells the container if it must unload the containing 
+	 * view when the dialog is closed. 
+	 * 
+	 * @param unloadViewOnClose unloadViewOnClose property value
+	 */
 	public void setUnloadViewOnClose(boolean unloadViewOnClose)
     {
     	this.unloadViewOnClose = unloadViewOnClose;
     }
 
+	/**
+	 * Write the modal property value. This property tells the container if the opened dialog should be modal. 
+	 * @param modal modal property value
+	 */
 	public void setModal(boolean modal)
 	{
 		dialog.setModal(modal);
 	}
-	
+
+	/**
+	 * Read the modal property value. This property tells the container if the opened dialog should be modal. 
+	 * @return modal property value
+	 */
 	public boolean isModal()
 	{
 		return dialog.isModal();
 	}
-	
+
+	/**
+	 * Set the dialog dimensions.
+	 * @param width dialog width
+	 * @param height dialog height
+	 */
 	public void setSize(String width, String height)
 	{
 		dialog.setSize(width, height);
 	}
 
+	/**
+	 * Set the dialog width
+	 * @param width dialog width
+	 */
 	@Override
 	public void setWidth(String width)
 	{
 		dialog.setWidth(width);
 	}
 
+	/**
+	 * Set the dialog height
+	 * @param width dialog height
+	 */
 	@Override
 	public void setHeight(String height)
 	{
 		dialog.setHeight(height);
 	}
-	
+
+	/**
+	 * Set the dialog position
+	 * @param left dialog left position
+	 * @param top dialog top position
+	 */
 	public void setPosition(int left, int top)
 	{
 		dialog.setPopupPosition(left, top);
@@ -191,6 +286,9 @@ public class DialogViewContainer extends SingleViewContainer
 		dialog.setStylePrimaryName(style);
 	}
 
+	/**
+	 * Open the dialog container and center it on the screen. If the container is already opened, just center it on the screen.
+	 */
 	public void center()
 	{
 		assert(innerView != null):"There is no View loaded into this container.";
@@ -198,11 +296,18 @@ public class DialogViewContainer extends SingleViewContainer
 		dialog.center();
 	}
 	
+	/**
+	 * Read the current view loaded into this container.
+	 * @return current view loaded into this container.
+	 */
 	public View getView()
 	{
 		return innerView;
 	}
 	
+	/**
+	 * Open the container dialog box. Assert that you previously loaded a view inside the container. 
+	 */
 	public void openDialog()
 	{
 		assert(innerView != null):"There is no View loaded into this container.";
@@ -210,11 +315,22 @@ public class DialogViewContainer extends SingleViewContainer
 		dialog.show();
 	}
 	
+	/**
+	 * Close the container dialog.
+	 */
 	public void closeDialog()
 	{
 		closeDialog(unloadViewOnClose);
 	}
-	
+
+	/**
+	 * Close the container dialog. If unloadView parameter is true, try to unload the current view first.
+	 * <p> If this view can not be unloaded (its unload event handler cancel the event), 
+	 * this method does not close the container and returns false.</p>
+	 * 
+	 * @param unloadView if true, unload the current view before close the container.
+	 * @return if the dialog was closed.
+	 */
 	public boolean closeDialog(boolean unloadView)
 	{
 		if (unloadView)
