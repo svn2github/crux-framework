98968f2d26c83d385d88972bd1888647bea1da7a
samuel@cruxframework.org <samuel@cruxframework.org@a5d2bbaa-053c-11de-b17c-0f1ef23b492c>
2013-12-09 16:06:44 +0000

    ENHANCEMENT adding widget defaultStyleName
    git-svn-id: http://crux-framework.googlecode.com/svn@4337 a5d2bbaa-053c-11de-b17c-0f1ef23b492c

--- branches/5.0.0/Crux/src/ui/widgets/org/cruxframework/crux/widgets/client/stackmenu/StackMenuItem.java
+++ branches/5.0.0/Crux/src/ui/widgets/org/cruxframework/crux/widgets/client/stackmenu/StackMenuItem.java
@@ -266,127 +266,18 @@ public class StackMenuItem extends Composite
 	}
 	
 	/**
-	 * @param style
+	 * @return the menu title widget. 
 	 */
-	public void setMenuTitleStyleName(String style) 
+	public VerticalPanel getMenuTitle() 
 	{
-		super.setStyleName(style);
+		return wrappingCanvas;
 	}
 	
 	/**
-	 * @see com.google.gwt.user.client.ui.UIObject.setStyleDependentName(String, boolean)
-	 * Applied to MenuTitle.
+	 * @return the menu item widget.
 	 */
-	public void setMenuTitleStyleDependentName(String styleSuffix, boolean add) 
+	public FlowPanel getMenuItem() 
 	{
-		super.setStyleDependentName(styleSuffix, add);
-	}
-	
-	/**
-	 * @see org.cruxframework.crux.widgets.client.stackmenu.StackMenuItem.setMenuTitleStylePrimaryName(String)
-	 * Applied to MenuTitle.
-	 */
-	public void setMenuTitleStylePrimaryName(String style) 
-	{
-		super.setStylePrimaryName(style);
-	}
-	
-	/**
-	 * @see org.cruxframework.crux.widgets.client.stackmenu.StackMenuItem.removeMenuTitleStyleDependentName(String)
-	 * Applied to MenuTitle.
-	 */
-	public void removeMenuTitleStyleDependentName(String styleSuffix) 
-	{
-		super.removeStyleDependentName(styleSuffix);
-	}
-	
-	/**
-	 * @see org.cruxframework.crux.widgets.client.stackmenu.StackMenuItem.removeMenuTitleStyleName(String)
-	 * Applied to MenuTitle.
-	 */
-	public void removeMenuTitleStyleName(String style) 
-	{
-		super.removeStyleName(style);
-	}
-	
-	/**
-	 * @see com.google.gwt.user.client.ui.UIObject.addStyleName(String)
-	 * Applied to MenuTitle.
-	 */
-	public void addMenuTitleStyleName(String style) 
-	{
-		super.addStyleName(style);
-	}
-	
-	/**
-	 * @see com.google.gwt.user.client.ui.UIObject.addStyleDependentName(String)
-	 * Applied to MenuTitle.
-	 */
-	public void addMenuTitleStyleDependentName(String styleSuffix) 
-	{
-		super.addStyleDependentName(styleSuffix);
-	}
-	
-	/**
-	 * @see org.cruxframework.crux.widgets.client.stackmenu.StackMenuItem.setMenuItemStyleName(String)
-	 * Applied to MenuItem.
-	 */
-	public void setMenuItemStyleName(String style) 
-	{
-		subItemsCanvas.setStyleName(style);
-	}
-	
-	/**
-	 * @see com.google.gwt.user.client.ui.UIObject.setStyleDependentName(String, boolean)
-	 * Applied to MenuItem.
-	 */
-	public void setMenuItemStyleDependentName(String styleSuffix, boolean add) 
-	{
-		subItemsCanvas.setStyleDependentName(styleSuffix, add);
-	}
-	
-	/**
-	 * @see com.google.gwt.user.client.ui.UIObject.setStylePrimaryName(String)
-	 * Applied to MenuItem.
-	 */
-	public void setMenuItemStylePrimaryName(String style) 
-	{
-		subItemsCanvas.setStylePrimaryName(style);
-	}
-	
-	/**
-	 * @see com.google.gwt.user.client.ui.UIObject.removeStyleDependentName(String)
-	 * Applied to MenuItem.
-	 */
-	public void removeMenuItemStyleDependentName(String styleSuffix) 
-	{
-		subItemsCanvas.removeStyleDependentName(styleSuffix);
-	}
-	
-	/**
-	 * @see com.google.gwt.user.client.ui.UIObject.removeStyleName(String)
-	 * Applied to MenuItem.
-	 */
-	public void removeMenuItemStyleName(String style) 
-	{
-		subItemsCanvas.removeStyleName(style);
-	}
-	
-	/**
-	 * @see com.google.gwt.user.client.ui.UIObject.addStyleDependentName(String)
-	 * Applied to MenuItem.
-	 */
-	public void addMenuItemStyleDependentName(String styleSuffix) 
-	{
-		subItemsCanvas.addStyleDependentName(styleSuffix);
-	}
-	
-	/**
-	 * @see com.google.gwt.user.client.ui.UIObject.addStyleName(String)
-	 * Applied to MenuItem.
-	 */
-	public void addMenuItemStyleName(String style) 
-	{
-		subItemsCanvas.addStyleName(style);
+		return subItemsCanvas;
 	}
 }
