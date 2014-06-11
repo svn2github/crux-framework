8a48924da97dc5fc11f0f54ce11508899246e97f
alexandre@triggolabs.com <alexandre@triggolabs.com@a5d2bbaa-053c-11de-b17c-0f1ef23b492c>
2014-06-11 18:56:54 +0000

    IGNORE - creating new branch
    git-svn-id: http://crux-framework.googlecode.com/svn@5184 a5d2bbaa-053c-11de-b17c-0f1ef23b492c

---
+++ branches/components/crux-smart-faces/1.1/src/main/java/org/cruxframework/crux/smartfaces/client/list/AbstractList.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2014 cruxframework.org.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package org.cruxframework.crux.smartfaces.client.list;
+
+import org.cruxframework.crux.core.client.utils.StringUtils;
+
+import com.google.gwt.user.client.ui.ComplexPanel;
+
+/**
+ * @author Samuel Almeida Cardoso (samuel@cruxframework.org)
+ *
+ */
+abstract class AbstractList extends ComplexPanel 
+{
+	protected String className;
+
+	public AbstractList(String className) 
+	{
+		this();
+		this.className = className;
+	}
+	
+	protected abstract void setElement();
+	
+	protected abstract String getDefaultClassName();
+	
+	public AbstractList() 
+	{
+		setElement();
+		
+		if(!StringUtils.isEmpty(className) || !StringUtils.isEmpty(getDefaultClassName()))
+		{
+			getElement().setClassName(StringUtils.isEmpty(className) ? getDefaultClassName() : className);
+		}
+	}
+
+	public void add(ListItem w) 
+	{
+		super.add(w, getElement());
+	}
+
+	public void insert(ListItem w, int beforeIndex) 
+	{
+		super.insert(w, getElement(), beforeIndex, true);
+	}
+}
---
+++ branches/components/crux-smart-faces/1.1/src/main/java/org/cruxframework/crux/smartfaces/client/menu/MenuItem.java
@@ -0,0 +1,116 @@
+/*
+ * Copyright 2014 cruxframework.org.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package org.cruxframework.crux.smartfaces.client.menu;
+
+import java.util.ArrayList;
+
+import org.cruxframework.crux.smartfaces.client.list.ListItem;
+import org.cruxframework.crux.smartfaces.client.list.UnorderedList;
+import org.cruxframework.crux.smartfaces.client.panel.NavPanel;
+
+import com.google.gwt.user.client.Element;
+import com.google.gwt.user.client.ui.Widget;
+
+/**
+ * A cross device menu item
+ * @author Samuel Almeida Cardoso (samuel@cruxframework.org)
+ */
+public class MenuItem
+{
+	private Element item;
+	private Widget widget;
+	private ArrayList<MenuItem> children;
+	private boolean root;
+	
+	public boolean isRoot()
+	{
+		return root;
+	}
+	
+	public MenuItem()
+	{
+		this(null);
+	}
+	
+	public MenuItem(Widget widget)
+	{
+		if(widget == null)
+		{
+			NavPanel navPanel = new NavPanel();
+			this.root = true;
+			this.widget = navPanel;
+			this.item = navPanel.getElement();
+			return;
+		}
+		
+		this.widget = widget;
+		ListItem li = new ListItem();
+		li.add(widget);
+		item = li.getElement();
+	}
+
+	public void add(MenuItem w) 
+	{
+		if(children == null)
+		{
+			children = new ArrayList<MenuItem>();
+		}
+		
+		if(children.isEmpty())
+		{
+			UnorderedList ul = new UnorderedList();
+			ul.getElement().appendChild(w.item);
+			item.appendChild(ul.getElement()); 
+		} else
+		{
+			item.getFirstChild().appendChild(w.item);
+		}
+		
+		children.add(w);
+	}
+
+	public void clear() 
+	{
+		item.removeFromParent();
+		children.clear();
+	}
+
+	public void remove(MenuItem w) 
+	{
+		w.item.removeFromParent();
+		children.remove(w);
+	}
+
+	public ArrayList<MenuItem> getChildren() 
+	{
+		return children;
+	}
+
+	public Widget getWidget() 
+	{
+		return widget;
+	}
+	
+	public void addClass(String className)
+	{
+		item.addClassName(className);
+	}
+	
+	public void removeClass(String className)
+	{
+		item.removeClassName(className);
+	}
+}
