7599e548a37f5894c9f65adb6de13bf04897060a
alexandre@triggolabs.com <alexandre@triggolabs.com@a5d2bbaa-053c-11de-b17c-0f1ef23b492c>
2014-08-06 18:46:00 +0000

    IGNORE - removing branch
    git-svn-id: http://crux-framework.googlecode.com/svn@5418 a5d2bbaa-053c-11de-b17c-0f1ef23b492c

--- branches/components/crux-widgets/5.2/src/main/java/org/cruxframework/crux/widgets/client/event/row/BeforeSaveRowEditionHandler.java
+++
@@ -1,8 +0,0 @@
-package org.cruxframework.crux.widgets.client.event.row;
-
-import com.google.gwt.event.shared.EventHandler;
-
-public interface BeforeSaveRowEditionHandler extends EventHandler{
-	
-	void onBeforeSaveRowEdition(BeforeSaveRowEditionEvent event);
-}
