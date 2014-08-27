c678d8e499a74d236ee6e590dc634c8e28da9b0d
alexandre@triggolabs.com <alexandre@triggolabs.com@a5d2bbaa-053c-11de-b17c-0f1ef23b492c>
2014-08-27 00:36:06 +0000

    IGNORE - removing branch
    git-svn-id: http://crux-framework.googlecode.com/svn@5505 a5d2bbaa-053c-11de-b17c-0f1ef23b492c

--- branches/components/crux-widgets/5.2/src/main/java/org/cruxframework/crux/widgets/client/event/row/HasBeforeSaveRowEditionHandler.java
+++
@@ -1,8 +0,0 @@
-package org.cruxframework.crux.widgets.client.event.row;
-
-import com.google.gwt.event.shared.HandlerRegistration;
-import com.google.gwt.event.shared.HasHandlers;
-
-public interface HasBeforeSaveRowEditionHandler extends HasHandlers  {
-	HandlerRegistration addBeforeSaveRowEditionHandler(BeforeSaveRowEditionHandler handler);
-}
