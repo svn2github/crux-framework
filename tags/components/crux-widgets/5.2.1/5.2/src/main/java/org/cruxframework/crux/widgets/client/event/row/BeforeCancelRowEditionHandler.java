3c2c26d822d2d1cc0485b65a501aaf6f054c01b0
samuel@cruxframework.org <samuel@cruxframework.org@a5d2bbaa-053c-11de-b17c-0f1ef23b492c>
2014-11-18 21:56:40 +0000

    IGNORE removing wrong tag
    git-svn-id: http://crux-framework.googlecode.com/svn@5833 a5d2bbaa-053c-11de-b17c-0f1ef23b492c

--- tags/components/crux-widgets/5.2.1/5.2/src/main/java/org/cruxframework/crux/widgets/client/event/row/BeforeCancelRowEditionHandler.java
+++
@@ -1,8 +0,0 @@
-package org.cruxframework.crux.widgets.client.event.row;
-
-import com.google.gwt.event.shared.EventHandler;
-
-public interface BeforeCancelRowEditionHandler extends EventHandler
-{
-	void onBeforeCancelRowEdition(BeforeCancelRowEditionEvent event);
-}
--- tags/components/crux-widgets/5.2.1/5.2/src/main/java/org/cruxframework/crux/widgets/client/event/row/HasCancelRowEditionHandler.java
+++
@@ -1,9 +0,0 @@
-package org.cruxframework.crux.widgets.client.event.row;
-
-import com.google.gwt.event.shared.HandlerRegistration;
-import com.google.gwt.event.shared.HasHandlers;
-
-public interface HasCancelRowEditionHandler extends HasHandlers
-{
-	HandlerRegistration addCancelRowEditionHandler(CancelRowEditionHandler handler);
-}
