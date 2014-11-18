3028f2ffa0aaea283c5ad3d977e5026919112f8f
samuel@cruxframework.org <samuel@cruxframework.org@a5d2bbaa-053c-11de-b17c-0f1ef23b492c>
2014-11-18 21:53:35 +0000

    IGNORE removing wrong tag
    git-svn-id: http://crux-framework.googlecode.com/svn@5831 a5d2bbaa-053c-11de-b17c-0f1ef23b492c

--- tags/components/crux-widgets/5.2.1/5.2/src/main/java/org/cruxframework/crux/widgets/client/event/row/CancelRowEditionHandler.java
+++
@@ -1,8 +0,0 @@
-package org.cruxframework.crux.widgets.client.event.row;
-
-import com.google.gwt.event.shared.EventHandler;
-
-public interface CancelRowEditionHandler extends EventHandler
-{
-	void onCancelRowEditionHandler(CancelRowEditionEvent event);
-}
